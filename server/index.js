b96fcb81a2b846bfe7f103e68d54284f3e4e27f8
imyashsingh <yash.singh5445@gmail.com>
2023-03-10 00:01:06 +0530

    completed all post fun and created front end

--- server/index.js
+++ server/index.js
@@ -12,8 +12,12 @@ import { fileURLToPath } from "url";
 import {register} from "./controllers/auth.js";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
-
-
+import postRoutes from "./routes/posts.js";
+import { verifyToken } from "./middleware/auth.js";
+import { createPost } from "./controllers/posts.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users,posts} from "./data/index.js";
 /* CONFIGRATION */
 
 const __filename = fileURLToPath(import.meta.url);
@@ -48,12 +52,15 @@ const upload = multer({storage});
 /* ROUTES WITH FILES */
 
 app.post("/auth/register", upload.single("picture"),register);
+app.post("/posts",verifyToken, upload.single("picture"),createPost);
+
 
 
 /*ROUTES*/
 
 app.use("/auth",authRoutes);
 app.use("/users",userRoutes);
+app.use("/posts",postRoutes);
 
 /*  MONGOOSE SETUP */
 
@@ -65,5 +72,9 @@ mongoose.connect(process.env.MONGO_URL,{
 })
 .then(()=>{
     app.listen(PORT,()=> console.log(`Server PORT : ${PORT}`));
+
+    /* ADD DATA ONE TIME */
+    //User.insertMany(users);
+    //Post.insertMany(posts);
 })
 .catch((error)=> console.log(`${error} did not connect`));
